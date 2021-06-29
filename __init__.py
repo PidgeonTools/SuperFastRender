949358895fdb6da1c7a87e593cab999f38547095
Kevin Lorengel <46607370+DrachenSeele@users.noreply.github.com>
2021-06-29 09:00:34 +0200

    Version Update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Super Fast Render (SFR)",
     "author": "Kevin Lorengel",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (2, 92, 0),
     "location": "Properties > Render > Super Fast Render",
     "description": "SFR optimizes your scene, so you render faster!",
